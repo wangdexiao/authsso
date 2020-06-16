dbd7148e8e02a85a687e212578fc800cb1d96013
wangdx <wangdx@hd100.com>
2020-06-16 15:09:28 +0800

    前端模板整合thymeleaf

--- authserver/src/main/java/com/sso/authserver/config/WebSecurityConfig.java
+++ authserver/src/main/java/com/sso/authserver/config/WebSecurityConfig.java
@@ -71,6 +71,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/webjars/**");
+        web.ignoring()
+                .antMatchers("/webjars/**")
+                .antMatchers("/assets/**")
+                ;
     }
 }
